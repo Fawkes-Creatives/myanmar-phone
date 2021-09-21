627268760d23ae5621a891bc1d7b5fb2041ef584
Naing Min Khant <naingminkhant.cm@gmail.com>
2021-09-21 23:42:14 +0630

    publish config

--- src/MyanmarPhoneServiceProvider.php
+++ src/MyanmarPhoneServiceProvider.php
@@ -14,8 +14,19 @@ class MyanmarPhoneServiceProvider extends ServiceProvider
     /* @var Application */
     protected $app;
 
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__ . '/config/myanmar_phone.php' => $this->app->configPath('myanmar_phone.php'),
+            ]);
+        }
+    }
+
     public function register()
     {
+        $this->mergeConfigFrom(__DIR__ . '/config/myanmar_phone.php', 'myanmar_phone');
+
         $this->app->bind(MyanmarPhoneContract::class, function ($app) {
             return $app->make(MyanmarPhone::class);
         });
