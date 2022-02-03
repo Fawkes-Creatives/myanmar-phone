a13742247afeb54156ff97076450267c621bf195
Tint Naing Win <amigo.k8@gmail.com>
2022-02-03 23:06:02 +0700

    add method comments

--- src/Facades/MyanPhone.php
+++ src/Facades/MyanPhone.php
@@ -6,8 +6,28 @@
 namespace MyanmarPhone\Facades;
 
 use Illuminate\Support\Facades\Facade;
-
 use MyanmarPhone\Contracts\MyanmarPhone;
+
+/**
+ * @method make(string $number))
+ * @method getPhoneInstance()
+ * @method getDataSource()
+ * @method setStrPhoneNumber(string $number)
+ * @method getStrPhoneNumber()
+ * @method getCountryCode()
+ * @method getPhoneNumber(bool $leadingZero = true)
+ * @method format($format = null)
+ * @method setSeparator($separator = null)
+ * @method getSeparator()
+ * @method response(string $number)
+ * @method formatE164()
+ * @method formatInternational()
+ * @method formatRFC3966(string $separator = null)
+ * @method formatNational(string $separator = null)
+ * @method check()
+ *
+ * @see \MyanmarPhone\MyanmarPhone
+ */
 
 class MyanPhone extends Facade
 {
