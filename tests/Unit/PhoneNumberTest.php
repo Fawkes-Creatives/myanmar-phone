08fc7d0ea59ce841fdfe29fc452b902d51087ef5
Tint Naing Win <amigo.k8@gmail.com>
2022-02-03 23:04:47 +0700

    update description

--- tests/Unit/PhoneNumberTest.php
+++ tests/Unit/PhoneNumberTest.php
@@ -6,27 +6,27 @@ use MyanmarPhone\Facades\MyanPhone;
 use MyanmarPhone\Helpers\DataSource;
 use MyanmarPhone\Tests\Helpers\Data;
 
-it('it_can_make', function () {
+it('can make', function () {
     $initCall = MyanPhone::make(Data::INTERNATIONAL_NUMBER);
 
     $this->assertInstanceOf(MyanmarPhone::class, $initCall);
 });
 
-it('its_config_default_format_is_e164', function () {
+it('is config default format is e164', function () {
     config()->set('myanmar_phone.default_format', PhoneNumberFormat::E164);
     $phone = MyanPhone::make(Data::LOCAL_NUMBER);
     $result = $phone->format();
     $this->assertStringStartsWith("+95", $result);
 });
 
-it('its_config_default_format_is_international', function () {
+it('is config default format is international', function () {
     config()->set('myanmar_phone.default_format', PhoneNumberFormat::INTERNATIONAL);
     $phone = MyanPhone::make(Data::NATIONAL_NUMBER);
     $result = $phone->format();
     $this->assertStringContainsString("+95 ", $result);
 });
 
-it('its_config_default_format_is_national', function () {
+it('is config default format is national', function () {
     config()->set('myanmar_phone.default_format', PhoneNumberFormat::NATIONAL);
     $phone = MyanPhone::make(Data::LOCAL_NUMBER);
     $result = $phone->format();
@@ -34,7 +34,7 @@ it('its_config_default_format_is_national', function () {
     $this->assertStringContainsString(" ", $result);
 });
 
-it('its_config_default_format_is_rfc3966', function () {
+it('is config default format is rfc3966', function () {
     config()->set('myanmar_phone.default_format', PhoneNumberFormat::RFC3966);
     $phone = MyanPhone::make(Data::LOCAL_NUMBER);
     $result = $phone->format();
@@ -42,22 +42,22 @@ it('its_config_default_format_is_rfc3966', function () {
     $this->assertStringContainsString("-", $result);
 });
 
-it('it_can_be_format', function () {
+it('can be format', function () {
     $result = MyanPhone::make(Data::LOCAL_NUMBER)->format();
     $this->assertIsString($result);
 });
 
-it('it_can_be_formatE164', function () {
+it('can be formatE164', function () {
     $result = MyanPhone::make(Data::LOCAL_NUMBER)->formatE164();
     $this->assertStringStartsWith("+95", $result);
 });
 
-it('it_can_be_formatInternational', function () {
+it('can be formatInternational', function () {
     $result = MyanPhone::make(Data::LOCAL_NUMBER)->formatInternational();
     $this->assertStringContainsString("+95 ", $result);
 });
 
-it('it_can_be_formatRFC3966', function () {
+it('can be formatRFC3966', function () {
     $result = MyanPhone::make(Data::LOCAL_NUMBER)->formatRFC3966();
     $this->assertStringStartsWith("tel", $result);
     $this->assertStringContainsString("-", $result);
@@ -66,7 +66,7 @@ it('it_can_be_formatRFC3966', function () {
     $this->assertStringNotContainsString("-", $result);
 });
 
-it('it_can_be_formatNational', function () {
+it('can be formatNational', function () {
     $result = MyanPhone::make(Data::LOCAL_NUMBER)->formatNational();
     $this->assertStringStartsWith("09 ", $result);
 
@@ -74,7 +74,7 @@ it('it_can_be_formatNational', function () {
     $this->assertStringContainsString("_", $result);
 });
 
-it('it_can_get_operator', function () {
+it('can get operator', function () {
     $result = MyanPhone::telecom(Data::MPT_NUMBER);
     $this->assertEquals(DataSource::MPT, $result);
 
@@ -91,61 +91,61 @@ it('it_can_get_operator', function () {
     $this->assertEquals(DataSource::MEC, $result);
 });
 
-it('it_telenor_is_true', function () {
+it('is telenor', function () {
     $result = MyanPhone::make(Data::TELENOR_NUMBER)->isTelenor();
 
     $this->assertTrue($result);
 });
 
-it('it_telenor_is_false', function () {
+it('is not telenor', function () {
     $result = MyanPhone::make(Data::MPT_NUMBER)->isTelenor();
 
     $this->assertFalse($result);
 });
 
-it('it_mpt_is_false', function () {
+it('is mpt', function () {
     $result = MyanPhone::make(Data::MPT_NUMBER)->isMPT();
 
     $this->assertTrue($result);
 });
 
-it('it_mpt_is_true', function () {
+it('is not mpt', function () {
     $result = MyanPhone::make(Data::OOREDOO_NUMBER)->isMPT();
 
     $this->assertFalse($result);
 });
 
-it('it_ooredoo_is_true', function () {
+it('is ooredoo', function () {
     $result = MyanPhone::isOoredoo(Data::OOREDOO_NUMBER);
 
     $this->assertTrue($result);
 });
 
-it('it_ooredoo_is_false', function () {
+it('is not ooredoo', function () {
     $result = MyanPhone::make(Data::TELENOR_NUMBER)->isOoredoo();
 
     $this->assertFalse($result);
 });
 
-it('it_mytel_is_true', function () {
+it('is mytel', function () {
     $result = MyanPhone::make(Data::MYTEL_NUMBER)->isMyTel();
 
     $this->assertTrue($result);
 });
 
-it('it_mytel_is_false', function () {
+it('is not mytel', function () {
     $result = MyanPhone::isMyTel(Data::TELENOR_NUMBER);
 
     $this->assertFalse($result);
 });
 
-it('it_mec_is_true', function () {
+it('is mec', function () {
     $result = MyanPhone::make(Data::MEC_NUMBER)->isMec();
 
     $this->assertTrue($result);
 });
 
-it('it_mec_is_false', function () {
+it('is not mec', function () {
     $result = MyanPhone::isMec(Data::OOREDOO_NUMBER);
 
     $this->assertFalse($result);
